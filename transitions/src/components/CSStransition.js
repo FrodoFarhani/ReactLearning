6dfa18cd18d0196d7700ae648b91cae10b6d0245
so0oshiance <m.farhani@rightel.ir>
2019-07-30 20:15:17 +0430

    CCTransitions

--- transitions/src/components/CSStransition.js
+++ transitions/src/components/CSStransition.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { CSSTransition } from "react-transition-group";
 
 import '../css/App.css';
 
 class Fade extends Component{
+    state={
+        show:true
+    }
+    showDiv=()=>{
+        this.setState({
+            show:!this.state.show 
+        })
+    }
     render(){
         return(
             <div>
-                CSS TRANSITIONS
+                {/* CSSTransition shoud get classNames and it will automatically appent 
+                    them to insite this tag element
+                    if you check the element in inspect html dom you can see by hitting
+                    the btn the classe square-entered-active and square-entered would 
+                    be attached to our DIV */}
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={2000}
+                    classNames="square"
+                >
+                    <div className={`square ${this.state.show}`}>Hello</div>
+                </CSSTransition>
+                 <div className="showDiv"
+                     onClick={this.showDiv}>
+                     Show/Hide DIV</div>
             </div>
         )
     }
