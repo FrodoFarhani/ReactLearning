6a735e7a4b851ab3705adc6c3d74b385c4c65f38
so0oshiance <m.farhani@rightel.ir>
2019-07-30 21:01:53 +0430

    snippets

---
+++ transitions/src/components/user.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+
+export default class user extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class user extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class user extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+user.propTypes = {
+
+};
+
+export default user;
+
+class user extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+import React from 'react';
+
+const user = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default user;
+
+import React, { Component } from 'react'
+
+export default class user extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
