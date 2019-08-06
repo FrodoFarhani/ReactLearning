3ed4b675737dd0617bbbe9dc2cb4a5e53388297b
so0oshiance <m.farhani@rightel.ir>
2019-08-06 11:49:32 +0430

    use ref or react to grab data

--- forms/src/components/controlled.js
+++ forms/src/components/controlled.js
@@ -3,13 +3,46 @@ import React, { Component } from 'react';
 class Controlled extends Component {
 
     state = {
+        name:'',
+        lastname:''  
+    }
+    handleNameChange=(event)=>{
+       console.log(event.target.value);
+       this.setState({
+            name:event.target.value
+       });
+    }
+    handleLastNameChange=(event)=>{
+       console.log(event.target.value); 
+       this.setState({
+            lastname:event.target.value
+       });
+    }
+    handleFormSubmit=(event)=>{
+        event.preventDefault();
+        console.log(this.state);
         
     }
-
     render(){
         return(
-            <div>
-                Controlled
+            <div className="container">
+               <form onSubmit={this.handleFormSubmit}>
+                   <div className="form_element">
+                       <label>Enter Name</label>
+                       <input type="text"
+                              value={this.state.name}
+                              onChange={this.handleNameChange}
+                        />
+                   </div>
+                   <div className="form_element">
+                       <label>Enter LastName</label>
+                       <input type="text"
+                              value={this.state.lastname}
+                              onChange={this.handleLastNameChange}
+                       />
+                   </div>
+                   <button type="submit">Register</button>
+               </form>
             </div>
         )
     }
--- forms/src/components/uncontrolled.js
+++ forms/src/components/uncontrolled.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 
 class Uncontrolled extends Component {
 
-    state = {
-
+    handleSubmitClick=(event)=>{
+        event.preventDefault();
+        const values={
+            name:this.name.value,
+            lastname:this.lastname.value
+        }
+        console.log(values);
+        
     }
 
     render(){
         return(
-            <div>
-                Uncontrolled
-            </div>
+            <div className="container">
+            <form>
+                <div className="form_element">
+                    <label>Enter Name</label>
+                    <input type="text"
+                           ref={input=>this.name=input}
+                     />
+                </div>
+                <div className="form_element">
+                    <label>Enter LastName</label>
+                    <input type="text"
+                           ref={input=>this.lastname=input}
+                    />
+                </div>
+                <button onClick={this.handleSubmitClick}>Register</button>
+            </form>
+         </div>
         )
     }
 }
